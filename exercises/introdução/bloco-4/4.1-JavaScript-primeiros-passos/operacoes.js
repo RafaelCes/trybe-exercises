44f00c378599597b75dcadde6cc37a4dd130b67e
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-03-24 16:04:17 -0300

    cria programa 1 operacoes

---
+++ exercises/introdução/bloco-4/4.1-JavaScript-primeiros-passos/operacoes.js
@@ -0,0 +1,12 @@
+let a = 5;
+let b = 12;
+let c = a + b;
+console.log("adição = " + c);
+c = a - b;
+console.log("subtração = " + c);
+c = a * b ;
+console.log("multiplicação = " + c);
+c = a / b;
+console.log("divisão = " + c);
+c = a % b;
+console.log("resto = " + c);
