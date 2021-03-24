4117ab192a18b90838433038e0a931997d647f77
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-03-24 17:52:25 -0300

    cria programa 10 calcula lucro

--- exercises/introdução/bloco-4/4.1-JavaScript-primeiros-passos/checaPar.js
+++ exercises/introdução/bloco-4/4.1-JavaScript-primeiros-passos/checaPar.js
@@ -2,7 +2,7 @@ let num1 = 6;
 let num2 = 2;
 let num3 = 4;
 
-if(num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0 ){
+if(num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0 ){
     console.log(true);
 }
 else{
