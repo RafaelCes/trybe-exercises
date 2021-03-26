471493c171bb42197fe201fb033744b1112db24f
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-03-26 15:29:55 -0300

    exercicio 2 cria triangulo

--- exercises/introdução/bloco-4/4.3/criaTriangulo.js
+++ exercises/introdução/bloco-4/4.3/criaTriangulo.js
@@ -1,4 +1,4 @@
-let entrada = 0;
+let entrada = 10;
 
 if(entrada < 1){
     console.log('erro n < 1');
@@ -7,7 +7,12 @@ if(entrada < 1){
 
 for(let coluna = 0; coluna < entrada; coluna +=1){
     for(let linha = 0; linha < entrada; linha +=1){
+        if(linha <= coluna){
         process.stdout.write("* ");
+        }
+        else{
+            process.stdout.write("  "); 
+        }
     }
     console.log('');
 }
