6d2557cfa8cbe1facd0afd580f09459b67125d4c
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-03-26 15:23:00 -0300

    exercicio 1 cria quadrado

---
+++ exercises/introdução/bloco-4/4.3/criaQuadrado.js
@@ -0,0 +1,8 @@
+let entrada = 10;
+
+for(let coluna = 0; coluna < entrada; coluna +=1){
+    for(let linha = 0; linha < entrada; linha +=1){
+        process.stdout.write("* ");
+    }
+    console.log('');
+}
