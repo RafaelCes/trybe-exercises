75c9253f31bb52ed14e78fc934fb7c7187824d85
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-04-05 14:58:11 -0300

    pratica parte 3 completa

---
+++ exercises/introdução/bloco-5/5.1/parteIII.js
@@ -0,0 +1,8 @@
+document.getElementById('header-container').style.background = 'lightgreen';
+document.getElementsByClassName('emergency-tasks')[0].style.background = 'orange';
+document.getElementsByClassName('no-emergency-tasks')[0].style.background = 'yellow';
+document.querySelectorAll('.emergency-tasks h3')[0].style.background = 'purple';
+document.querySelectorAll('.emergency-tasks h3')[1].style.background = 'purple';
+document.querySelectorAll('.no-emergency-tasks h3')[0].style.background = 'black';
+document.querySelectorAll('.no-emergency-tasks h3')[1].style.background = 'black';
+document.getElementById('footer-container').style.background = 'darkgreen';
