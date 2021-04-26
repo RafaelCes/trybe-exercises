515264288309ad95314c8897380eb14444b84bb1
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-04-26 15:57:33 -0300

    exercicios 1 2 3 4

---
+++ exercises/introdução/bloco-7/7.1/exercise.js
@@ -0,0 +1,77 @@
+
+
+
+
+// Agora você vai fazer alguns exercícios de fixação.
+// Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
+// Modifique a estrutura da função para que ela seja uma arrow function .
+// Modifique as concatenações para template literals .
+// Copie o código abaixo.
+// Copiar
+//       function testingScope(escopo) {
+//         if (escopo === true) {
+//           var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//           ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//           console.log(ifScope);
+//         } else {
+//           var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//           console.log(elseScope);
+//         }
+//         console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+//       }
+
+//       testingScope(true);
+
+function testingScope(escopo) {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    console.log(ifScope);
+  } else {
+    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+  
+}
+testingScope(true);
+
+
+// Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
+// Utilize template literals para que a chamada console.log(oddsAndEvens); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+// Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.
+// Copie o código abaixo.
+// Copiar
+//       const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+//       // Seu código aqui.
+
+//       console.log(oddsAndEvens);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const order = array => array.sort((num1, num2) => num1 - num2);
+order(oddsAndEvens);
+console.log(oddsAndEvens);
+
+// Crie uma função que receba um número e retorne seu fatorial.
+// Na matemática, o fatorial de um número não negativo N , com a notação N! , é o produto de todos os inteiros menores ou iguais a N . Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
+// Bônus (opcional): tente fazer o mesmo exercício de forma recursiva . Spoiler: É possível resolver com uma linha usando ternary operator .
+
+const factorial = number => number === 1 ? number : number * factorial(number - 1);
+console.log(factorial(6));
+
+// Crie uma função que receba uma frase e retorne qual a maior palavra.
+// Exemplo:
+// Copiar
+//       longestWord("Antônio foi no banheiro e não sabemos o que aconteceu") // retorna 'aconteceu'
+
+const longestWord = (string) => {
+  let array = [];
+  let longest = '';
+  array = string.split(' ');
+  for (let index in array) {
+    array[index].length > longest.length ? longest = array[index] : longest
+  }
+  return longest;
+}
+console.log(longestWord('antoniosssssss foi no banheiro e não sabemos o que aconteceu'));
