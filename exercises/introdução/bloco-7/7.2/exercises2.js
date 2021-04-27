0e71921a90b512d2d3adf57d78cb91b8f65fec1b
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-04-27 15:57:26 -0300

    exercicios 1 2 3 4 parte 2

--- exercises/introdução/bloco-7/7.2/exercises2.js
+++ exercises/introdução/bloco-7/7.2/exercises2.js
@@ -17,3 +17,23 @@ const lesson3 = {
   professor: 'Maria Clara',
   turno: 'noite',
 };
+
+const changeKey = (object, key, value) => object[key] = value;
+changeKey(lesson2, 'turno', 'manha');
+console.log(lesson2);
+
+const listKeys = object => Object.keys(object);
+console.log(listKeys(lesson1));
+
+const getLength = object => Object.keys(object).length;
+console.log(getLength(lesson2));
+
+const listValues = object => Object.values(object);
+console.log(listValues(lesson1));
+
+
+// Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+// Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+// Crie uma função para mostrar o tamanho de um objeto.
+// Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+// Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
