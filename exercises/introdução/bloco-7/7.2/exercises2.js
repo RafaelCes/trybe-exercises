c278c9a073517866ac2980937777197c3281144c
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-04-27 16:13:09 -0300

    exercicios 5 6 7 parte 2

--- exercises/introdução/bloco-7/7.2/exercises2.js
+++ exercises/introdução/bloco-7/7.2/exercises2.js
@@ -31,9 +31,50 @@ console.log(getLength(lesson2));
 const listValues = object => Object.values(object);
 console.log(listValues(lesson1));
 
+const allLessons = {
+  lesson1:{},
+  lesson2:{},
+  lesson3:{},
+}
+Object.assign(allLessons.lesson1, lesson1);
+Object.assign(allLessons.lesson2, lesson2);
+Object.assign(allLessons.lesson3, lesson3);
+
+console.log(allLessons);
+
+const studentsTotal = object => object.lesson1.numeroEstudantes + object.lesson2.numeroEstudantes + object.lesson3.numeroEstudantes;
+console.log(studentsTotal(allLessons));
+
+const getValueByNumber = (object, index) => Object.values(object)[index];
+console.log(getValueByNumber(lesson1, 0));
 
 // Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
 // Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
 // Crie uma função para mostrar o tamanho de um objeto.
 // Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
 // Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
+// console.log(allLessons);
+/*
+{
+  lesson1:
+   { materia: 'Matemática',
+     numeroEstudantes: 20,
+     professor: 'Maria Clara',
+     turno: 'manhã' },
+  lesson2:
+   { materia: 'História',
+     numeroEstudantes: 20,
+     professor: 'Carlos',
+     turno: 'noite' },
+  lesson3:
+   { materia: 'Matemática',
+     numeroEstudantes: 10,
+     professor: 'Maria Clara',
+     turno: 'noite' }
+};
+*/
+// Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+// Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
+// Copiar
+// console.log(getValueByNumber(lesson1, 0));
+// Output: 'Matématica'
