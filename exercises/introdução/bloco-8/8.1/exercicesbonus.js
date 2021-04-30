3c2b57c9024619a82877f85768afa6f028ac89a1
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-04-30 16:23:32 -0300

    exercicios bonus parte 2

--- exercises/introdução/bloco-8/8.1/exercicesbonus.js
+++ exercises/introdução/bloco-8/8.1/exercicesbonus.js
@@ -27,7 +27,7 @@ const rndDmg = (min, max) => Math.floor(Math.random() * (max - min + 1) ) + min;
 
 // Crie uma função que retorna o dano do dragão.
 // O dano será um número aleatório entre 15 (dano mínimo) e o valor do atributo strength (dano máximo).
-const DragonDmg = () => rndDmg(15, dragon.strength);
+const dragonDmg = () => rndDmg(15, dragon.strength);
 
 // Crie uma função que retorna o dano causado pelo warrior .
 // O dano será um número aleatório entre o valor do atributo strength (dano mínimo) e o valor de strength * weaponDmg (dano máximo).
@@ -35,12 +35,47 @@ const warriorDmg = () => rndDmg(warrior.strength, warrior.strength * warrior.wea
 
 // Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
 // O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
+// A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de dano recebe uma mensagem (Ex: "Não possui mana suficiente") e a mana gasta é 0.
 const mageDmg = () => {
-  mage.mana < 15 ? {damage: 'Não possui mana suficiente', mana: 0,} :
-  {
+  if (mage.mana < 15)  return {damage: 'Não possui mana suficiente', mana: 0,}; 
+  return {
     damage: rndDmg(mage.intelligence, mage.intelligence * 2),
     mana: 15,
   }
+
 };
 
-// A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de dano recebe uma mensagem (Ex: "Não possui mana suficiente") e a mana gasta é 0.
+// Agora que você já possui a implementação das funções relativas aos três exercícios anteriores, passe-as como parâmetro para outras funções que irão compor um objeto gameActions . O objeto será composto por ações do jogo e cada ação é por denifição uma HOF , pois neste caso, são funções que recebem como parâmetro outra função.
+// Copie o código abaixo e inicie sua implementação:
+
+// Crie a primeira HOF que compõe o objeto gameActions . Ela será a função que simula o turno do personagem warrior . Esta HOF receberá como parâmetro a função que calcula o dano deferido pelo personagem warrior e atualizará os healthPoints do monstro dragon . Além disto ela também deve atualizar o valor da chave damage do warrior .
+
+// Crie a segunda HOF que compõe o objeto gameActions . Ela será a função que simula o turno do personagem mage . Esta HOF receberá como parâmetro a função que calcula o dano deferido pelo personagem mage e atualizará os healthPoints do monstro dragon . Além disto ela também deve atualizar o valor das chaves damage e mana do mage.
+
+// Crie a terceira HOF que compõe o objeto gameActions . Ela será a função que simula o turno do monstro dragon . Esta HOF receberá como parâmetro a função que calcula o dano deferido pelo monstro dragon e atualizará os healthPoints dos personagens mage e warrior . Além disto ela também deve atualizar o valor da chave damage do monstro.
+
+// Adicione ao objeto gameActions uma função de primeira classe que retorna o objeto battleMembers atualizado e faça um console.log para visualizar o resultado final do turno.
+
+const gameActions = {
+  warriorTurn: (warriorAtack) => {
+    warrior.damage = warriorAtack();
+    dragon.healthPoints -= warrior.damage;
+  },
+  mageTurn: (mageAtack) => {
+    const spell = mageAtack();
+    mage.mana -= spell.mana;
+    mage.damage = spell.damage;
+    dragon.healthPoints -= mage.damage;
+  },
+  dragonTurn: (dragonAtack) => {
+    dragon.damage = dragonAtack();
+    mage.healthPoints -= dragon.damage;
+    warrior.healthPoints -= dragon.damage;
+  },
+  endTurn: () => battleMembers,
+};
+
+gameActions.warriorTurn(warriorDmg);
+gameActions.mageTurn(mageDmg);
+gameActions.dragonTurn(dragonDmg);
+console.log(gameActions.endTurn());
