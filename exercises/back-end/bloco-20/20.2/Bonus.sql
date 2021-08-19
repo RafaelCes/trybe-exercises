ce2a4582b9e211f2bbce65a596cb28c469997237
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-08-18 21:59:54 -0300

    beautify

--- exercises/back-end/bloco-20/20.2/Bonus.sql
+++ exercises/back-end/bloco-20/20.2/Bonus.sql
@@ -1,6 +1,26 @@
 use PiecesProviders;
-SELECT Piece, Price FROM Provides WHERE Provider = 'RBT';
-SELECT * FROM Provides ORDER BY Price LIMIT 5;
-SELECT Provider, Price FROM Provides ORDER BY Price DESC LIMIT 4 OFFSET 3;
-SELECT * FROM Provides WHERE Provider = 'HAL' ORDER BY Price DESC;
-SELECT COUNT(DISTINCT Provider) FROM Provides WHERE Piece = 1;
+
+SELECT  Piece
+       ,Price
+FROM Provides
+WHERE Provider = 'RBT';
+
+SELECT  *
+FROM Provides
+ORDER BY Price
+LIMIT 5;
+
+SELECT  Provider
+       ,Price
+FROM Provides
+ORDER BY Price DESC
+LIMIT 4 OFFSET 3;
+
+SELECT  *
+FROM Provides
+WHERE Provider = 'HAL'
+ORDER BY Price DESC;
+
+SELECT  COUNT(DISTINCT Provider)
+FROM Provides
+WHERE Piece = 1;
--- exercises/back-end/bloco-20/20.2/exercicios.sql
+++ exercises/back-end/bloco-20/20.2/exercicios.sql
@@ -1,16 +1,62 @@
 USE Scientists;
-SELECT 'This is SQL Exercise, Practice and Solution';
-SELECT '3','6','9';
-SELECT 10 + 15;
-SELECT 25 * 5 + 2;
-SELECT * FROM Scientists;
-SELECT Name AS 'Nome do Projeto', Hours AS 'Tempo de Trabalho' FROM Projects;
-SELECT Name FROM Scientists ORDER BY Name;
-SELECT Name FROM Projects ORDER BY Name DESC;
-SELECT 'O projeto ', Name, ' precisou de ', Hours, ' horas para ser concluído.' FROM Projects;
-SELECT Name , Hours  FROM Projects ORDER BY Hours DESC LIMIT 3;
-SELECT DISTINCT Project FROM AssignedTo;
-SELECT Name FROM Projects ORDER BY Hours DESC LIMIT 1;
-SELECT Name FROM Projects ORDER BY Hours LIMIT 1 OFFSET 1;
-SELECT * FROM Projects ORDER BY Hours LIMIT 5;
-SELECT 'Existem ', COUNT(*), ' cientistas na tabela Scientists.' FROM Scientists;
+
+SELECT  'This is SQL Exercise,Practice AND Solution';
+
+SELECT  '3'
+       ,'6'
+       ,'9';
+
+SELECT  10 + 15;
+
+SELECT  25 * 5 + 2;
+
+SELECT  *
+FROM Scientists;
+
+SELECT  Name  AS 'Nome do Projeto'
+       ,Hours AS 'Tempo de Trabalho'
+FROM Projects;
+
+SELECT  Name
+FROM Scientists
+ORDER BY Name;
+
+SELECT  Name
+FROM Projects
+ORDER BY Name DESC;
+
+SELECT  'O projeto '
+       ,Name
+       ,' precisou de '
+       ,Hours
+       ,' horas para ser concluído.'
+FROM Projects;
+
+SELECT  Name
+       ,Hours
+FROM Projects
+ORDER BY Hours DESC
+LIMIT 3;
+
+SELECT  DISTINCT Project
+FROM AssignedTo;
+
+SELECT  Name
+FROM Projects
+ORDER BY Hours DESC
+LIMIT 1;
+
+SELECT  Name
+FROM Projects
+ORDER BY Hours
+LIMIT 1 OFFSET 1;
+
+SELECT  *
+FROM Projects
+ORDER BY Hours
+LIMIT 5;
+
+SELECT  'Existem '
+       ,COUNT(*)
+       ,' cientistas na tabela Scientists.'
+FROM Scientists;
